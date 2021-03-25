e122c08a2aef79087f574c4c1a892082d0fb8e6c
durancicek <durancicek46@gmail.com>
2021-03-25 16:47:09 +0300

    Result Modeli oluşturuldu.

---
+++ CalisanTakipCommon/ResultModels/IResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalisanTakipCommon.ResultModels
+{
+    public interface IResult
+    {
+         bool IsSuccess { get; set; }
+         string Mesaj { get; set; }
+    }
+}
---
+++ CalisanTakipCommon/ResultModels/Result.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalisanTakipCommon.ResultModels
+{
+    public class Result<T> : IResult
+    {
+        public bool IsSuccess { get; set; }
+        public string Mesaj { get; set; }
+        public T Data { get; set; }
+        public int ToplamMiktar { get; set; }
+
+        public Result(bool isSuccess, string mesaj)
+            : this(isSuccess, mesaj, default(T))
+        {
+
+        }
+        public Result(bool isSuccess, string mesaj, T data)
+           : this(isSuccess, mesaj, data, 0)
+        {
+
+        }
+
+        public Result(bool isSuccess, string mesaj, T data,int toplamMiktar)
+        {
+            IsSuccess = isSuccess;
+            Mesaj = mesaj;
+            Data = data;
+            ToplamMiktar = toplamMiktar;
+        }
+
+
+
+
+
+    }
+}
